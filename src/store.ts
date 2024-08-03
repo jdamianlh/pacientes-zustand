5fbe140338e89ebccc6dfd30eea319f9fb76cd7e
Damian <jdamianlh@gmail.com>
2024-08-02 21:44:40 -0500

    modify store

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,22 @@
 import { create } from "zustand"
+import {devtools} from "zustand/middleware"
 import {v4 as uuidv4} from 'uuid'
 import { DraftPatient, Patient } from "./types"
 
 type PatientState = {
     patients: Patient[]
+    activeId: Patient['id']
     addPatient: (data: DraftPatient) => void
+    deletePatient: (id: Patient['id']) => void
+    getPatientById: (id: Patient['id']) => void
 }
 const createPatient = (patient:DraftPatient) : Patient => {
     return {...patient, id: uuidv4()}
 }
 
-export const usePatientStore = create<PatientState>((set) => ({
+export const usePatientStore = create<PatientState>()(devtools((set) => ({
     patients : [],
+    activeId: '',
     addPatient: (data) =>{
 
         const newPatient = createPatient(data)
@@ -19,5 +24,17 @@ export const usePatientStore = create<PatientState>((set) => ({
         set((state)=> ({
             patients: [...state.patients, newPatient]
         }))
+    },
+    deletePatient: (id) => {
+        set((state) => ({
+            patients: state.patients.filter(patient => patient.id !== id)
+        }))
+        
+    },
+    getPatientById: (id) => {
+        set(() =>({
+            activeId: id
+        }))
+        
     }
-}))
+})))
